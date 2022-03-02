8d562d7cbeeba6cb03d96e1408a667efd6df2e9b
Murat Karasah <mkarasah@gmail.com>
2022-03-02 14:15:04 +0300

    the end

---
+++ AbstractFactory/Class/Besiktas.cs
@@ -0,0 +1,22 @@
+﻿using AbstractFactory.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Class
+{
+    public class Besiktas : ITeam
+    {
+        public string getStadium()
+        {
+            return "Vodofone Park";
+        }
+
+        public string getTeamColor()
+        {
+            return "Siyah-Beyaz";
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,16 @@
+﻿using AbstractFactory.Class;
+using AbstractFactory.Interface;
+using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        //İlişkili sınıfların oluşturulmasında kullanılır. Farklı ürünlerin fabrikalarının soyutlanması olarak tanımlanabilir. Tekrar eden if else yapısından kurtarır.
+        static void Main(string[] args)
+        {
+            ITeam besiktas = TeamFactory.GetTeam(new BesiktasFactory());
+            Console.WriteLine("Beşiktaşın Stadı : {0}\nrenkleri : {1}",besiktas.getStadium(),besiktas.getTeamColor());
+        }
+    }
+}
